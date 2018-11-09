927096e9c3357a6c475a6a6506c222465f351789
Huikie <z4everaz@hotmail.com>
2018-11-09 23:38:59 +0100

    Landscape mode

--- app/src/main/java/com/example/daan/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/daan/mrpotatohead/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         // to make sure only the body is displayed at first
         ImageView body = findViewById(R.id.body);
         body.setVisibility(View.VISIBLE);
